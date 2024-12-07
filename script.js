c404b42c63986c122b916469eba16dd90acf5f34
Starmology <oreo.milk.mouse@gmail.com>
2024-12-07 16:02:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var rndRarity
+var rndIndex 
+
+function openPack(){
+  document.getElementById("btnOpenPack").disabled = true
+  document.getElementById("picPack").src="https://millertime72.github.io/PookGame/images/PookPackOpened.png";
+  rndRarity = Math.floor(Math.random() * 101);
+  if (rndRarity < 80) {//Common
+    document.getElementById("txtRarityPulled").innerText = "Common";
+    rndIndex = Math.floor(Math.random() * 3) + 1;
+    if (rndIndex == 1){
+      document.getElementById("txtPookPulled").innerText = "Chick";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/common/CommonChick.png";
+    }else if (rndIndex==2){
+      document.getElementById("txtPookPulled").innerText = "Dog";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/common/CommonDog.png";
+    } else if (rndIndex==3){
+      document.getElementById("txtPookPulled").innerText = "Panda";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/common/CommonPanda.png";
+    }
+  } else if (rndRarity > 95){//Legendary
+      document.getElementById("txtRarityPulled").innerText = "Legendary";
+      Math.floor(Math.random() * 3) + 1;
+    if (rndIndex==1){
+      document.getElementById("txtPookPulled").innerText = "Carrot Rabbit";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/legendary/LegendaryCarrotRabbit.png";
+    }else if (rndIndex==2){
+      document.getElementById("txtPookPulled").innerText = "Sheep";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/legendary/LegendarySheep.png";
+    }else if (rndIndex==3){
+      document.getElementById("txtPookPulled").innerText = "Sloth";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/legendary/LegendarySloth.png";
+    }
+  }else{//Rare
+    document.getElementById("txtRarityPulled").innerText = "Rare";
+    rndIndex = Math.floor(Math.random() * 3) + 1;
+    if (rndIndex==1){
+      document.getElementById("txtPookPulled").innerText = "Turnip Cat";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/rare/RareTurnipCat.png";
+    }else if (rndIndex==2){
+      document.getElementById("txtPookPulled").innerText = "Pig";
+      document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/rare/RarePig.png";
+    } else if (rndIndex==3){
+     document.getElementById("txtPookPulled").innerText = "Frog";
+     document.getElementById("picPookPulled").src = "https://millertime72.github.io/PookGame/images/rare/RareFrog.png";
+    }
+  }
+}
